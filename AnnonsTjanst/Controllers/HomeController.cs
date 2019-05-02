a7a7b809df4baaa78102484a96be6e646661de93
Emil Gummesson <emgu0012@student.hv.se>
2019-05-02 12:56:46 +0200

    Add project files.

---
+++ AnnonsTjanst/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AnnonsTjanst.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ServiceReference1.Service1Client client = new ServiceReference1.Service1Client();
+            ViewBag.testString = client.Test();
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
